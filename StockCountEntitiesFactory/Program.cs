774e3127a064e3484f2f0262f0be5309a72b8a1e
Andrey Shchedrin <shedrin2001@mail.ru>
2024-02-24 17:12:44 +0700

    Update solution

--- StockCountEntitiesFactory/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Meetup.Demo.Factory;
-
-Console.WriteLine("Hello, World!");
-
-var store = DocumentStoreHolder.Store;
-
-using (var session = store.OpenSession()) { }
